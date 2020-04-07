3d4b21e9d45f2da4fb18863f61690aaa68b9cc09
andatu <andre.aturai@outlook.com>
2020-04-07 19:49:28 +0200

    andres 2 commit

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -8,12 +8,11 @@ import java.awt.event.*;
 public class Menu extends JFrame{
    private Controller contr;
     private JMenuBar menuBar;
-    private JMenu menu, submenu;
     private JMenuItem menuItem;
     private JRadioButtonMenuItem rbMenuItem;
     private JCheckBoxMenuItem cbMenuItem;
-    private JMenu  edit, view;
-    private JMenuItem newItem, openItem, saveItem, exitItem, fontItem;
+    //private JMenu view, toolWindow;
+    //private JMenuItem newItem, openItem, saveItem, exitItem, fontItem;
 
 
     public Menu(Controller contr){
@@ -30,13 +29,13 @@ public class Menu extends JFrame{
         menuBar = new JMenuBar();
 
         //Build the scroll-down menus.
-        menu = new JMenu("File");
-        menu.setMnemonic(KeyEvent.VK_A);
-        menu.getAccessibleContext().setAccessibleDescription(
+        JMenu file = new JMenu("File");
+        file.setMnemonic(KeyEvent.VK_A);
+        file.getAccessibleContext().setAccessibleDescription(
                 "The only menu in this program that has menu items");
-        edit = new JMenu("Edit ");
-        view= new JMenu("view ");
-        menuBar.add(menu);
+        JMenu edit = new JMenu("Edit");
+        JMenu view = new JMenu("View");
+        menuBar.add(file);
         menuBar.add(edit);
         menuBar.add(view);
 
@@ -45,10 +44,30 @@ public class Menu extends JFrame{
         JMenuItem newItem1 = new JMenuItem("Open");
         JMenuItem newItem2 = new JMenuItem("Save");
         JMenuItem newItem3 = new JMenuItem("Save As..");
-        menu.add(newItem0);
-        menu.add(newItem1);
-        menu.add(newItem2);
-        menu.add(newItem3);
+        file.add(newItem0);
+        file.add(newItem1);
+        file.add(newItem2);
+        file.add(newItem3);
+
+        JMenuItem undo = new JMenuItem("Undo");
+        JMenuItem redo = new JMenuItem("Redo");
+        JMenuItem cut = new JMenuItem("Cut");
+        JMenuItem copy = new JMenuItem("Copy");
+        JMenuItem paste = new JMenuItem("Paste");
+        edit.add(undo);
+        edit.add(redo);
+        edit.add(cut);
+        edit.add(copy);
+        edit.add(paste);
+
+        JMenuItem editWindow = new JMenuItem("Edit Window");
+        JMenuItem fontWindow = new JMenuItem("Font Window");
+        JMenu toolWindow = new JMenu("Tool Windows");
+        view.add(toolWindow);
+        toolWindow.add(editWindow);
+        toolWindow.add(fontWindow);
+
+
 
       /*  menuBar = new JMenuBar();
         fileMenu = new JMenu(" File ");
