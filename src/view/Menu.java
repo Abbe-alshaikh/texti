31d984ac91d0da238207ca755ee61dfc31d5aedc
abbe-alshaikh <abbe.alshaikh@gmail.com>
2020-04-08 16:46:18 +0200

    Updaterat meny knappar

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -6,13 +6,15 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class Menu extends JFrame{
-   private Controller contr;
+    private Controller contr;
     private JMenuBar menuBar;
     private JMenuItem menuItem;
     private JRadioButtonMenuItem rbMenuItem;
     private JCheckBoxMenuItem cbMenuItem;
-    //private JMenu view, toolWindow;
-    //private JMenuItem newItem, openItem, saveItem, exitItem, fontItem;
+    private JMenu file, view, toolWindow, edit, insert, formatting, help, print, alignment, lists;
+    private JMenuItem newItem, openItem, saveItem,saveAsItem,undo, redo, cut, copy, paste, spell_check,
+            editWindow, fontWindow, picture, table, doBold, cursive, size_of_letters, underline,
+            highlighting, left, center, right, bullet_points, enumeration, fonts ;
 
 
     public Menu(Controller contr){
@@ -29,76 +31,96 @@ public class Menu extends JFrame{
         menuBar = new JMenuBar();
 
         //Build the scroll-down menus.
-        JMenu file = new JMenu("File");
+        file = new JMenu("File");
         file.setMnemonic(KeyEvent.VK_A);
         file.getAccessibleContext().setAccessibleDescription(
                 "The only menu in this program that has menu items");
-        JMenu edit = new JMenu("Edit");
-        JMenu view = new JMenu("View");
+        edit = new JMenu("Edit");
+        view = new JMenu("View");
+        insert = new JMenu("Insert");
+        formatting = new JMenu("Text Formatting");
+        help = new JMenu("Help");
+        print = new JMenu("Print");
+
+
         menuBar.add(file);
         menuBar.add(edit);
         menuBar.add(view);
+        menuBar.add(insert);
+        menuBar.add(formatting);
+        menuBar.add(help);
+        menuBar.add(print);
+
 
         //add items to File scroll-down menu
-        JMenuItem newItem0 = new JMenuItem("New");
-        JMenuItem newItem1 = new JMenuItem("Open");
-        JMenuItem newItem2 = new JMenuItem("Save");
-        JMenuItem newItem3 = new JMenuItem("Save As..");
-        file.add(newItem0);
-        file.add(newItem1);
-        file.add(newItem2);
-        file.add(newItem3);
+        newItem = new JMenuItem("New");
+        openItem = new JMenuItem("Open");
+        saveItem = new JMenuItem("Save");
+        saveAsItem = new JMenuItem("Save As..");
+        file.add(newItem);
+        file.add(openItem);
+        file.add(saveItem);
+        file.add(saveAsItem);
 
-        JMenuItem undo = new JMenuItem("Undo");
-        JMenuItem redo = new JMenuItem("Redo");
-        JMenuItem cut = new JMenuItem("Cut");
-        JMenuItem copy = new JMenuItem("Copy");
-        JMenuItem paste = new JMenuItem("Paste");
+        undo = new JMenuItem("Undo");
+        redo = new JMenuItem("Redo");
+        cut = new JMenuItem("Cut");
+        copy = new JMenuItem("Copy");
+        paste = new JMenuItem("Paste");
+        spell_check = new JMenuItem("Spell check");
         edit.add(undo);
         edit.add(redo);
         edit.add(cut);
         edit.add(copy);
         edit.add(paste);
+        edit.add(spell_check);
 
-        JMenuItem editWindow = new JMenuItem("Edit Window");
-        JMenuItem fontWindow = new JMenuItem("Font Window");
-        JMenu toolWindow = new JMenu("Tool Windows");
+        editWindow = new JMenuItem("Edit Window");
+        fontWindow = new JMenuItem("Font Window");
+        toolWindow = new JMenu("Tool Windows");
         view.add(toolWindow);
         toolWindow.add(editWindow);
         toolWindow.add(fontWindow);
 
+        picture = new JMenuItem("Insert Picture");
+        insert.add(picture);
+        table = new JMenuItem("Insert Table");
+        insert.add(table);
 
+        doBold = new JMenuItem("Bold");
+        formatting.add(doBold);
 
-      /*  menuBar = new JMenuBar();
-        fileMenu = new JMenu(" File ");
-        editMenu = new JMenu("Edit ");
-        viewMenu = new JMenu("View ");
+        cursive = new JMenuItem("Cursive");
+        formatting.add(cursive);
 
-        newMenuItem = new JMenuItem("New");
-        fileMenu.add(newMenuItem);
-        fileMenu.addSeparator();
-        fileMenu.setMnemonic('f');
+        size_of_letters = new JMenuItem("Size");
+        formatting.add(size_of_letters);
 
-        openMenuItem = new JMenuItem("Open");
+        underline = new JMenuItem("Underline");
+        formatting.add(underline);
 
-        fileMenu.add(openMenuItem);
+        highlighting = new JMenuItem("Highlighting");
+        formatting.add(highlighting);
 
-        saveMenuItem = new JMenuItem("Save");
-        fileMenu.add(saveMenuItem);
+        alignment = new JMenu ("Alignment ");
+        formatting.add(alignment);
+        left = new JMenuItem("Left ");
+        center = new JMenuItem("Center ");
+        right = new JMenuItem("Right ");
+        alignment.add(left);
+        alignment.add(center);
+        alignment.add(right);
 
-        fileMenu.addSeparator();
-        exitMenuItem = new JMenuItem("Exit");
-        fileMenu.add(exitMenuItem);
+        lists = new JMenu ("Lists");
+        bullet_points = new JMenuItem("Bullet Points");
+        enumeration = new JMenuItem("Enumeration");
+        formatting.add(lists);
+        lists.add(bullet_points);
+        lists.add(enumeration);
 
-        FontMenuItem = new JMenuItem("Font");
-        editMenu.add(FontMenuItem);
-        menuBar.add(fileMenu);
-        menuBar.add(editMenu);
-        menuBar.add(viewMenu);
+        fonts = new JMenuItem("Fonts");
+        formatting.add(fonts);
 
-        this.setJMenuBar(menuBar);
-
-     */
     }
    //public void openFile(){
     //    TextView tv = contr.openFile();
