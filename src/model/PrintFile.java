3b1baa9d482b677e09cf29334653eb9d8a366fdf
adamgus <43177666+adamgus@users.noreply.github.com>
2020-05-14 15:25:33 +0200

    Cleaned up printing

--- src/model/PrintFile.java
+++ src/model/PrintFile.java
@@ -9,30 +9,16 @@ import java.awt.print.PrinterException;
 public class PrintFile{
 
     public void printSetup(JTextPane ta){
-        //PrinterJob job = PrinterJob.getPrinterJob();
-        //job.setPrintable(ta.getPrintable(null,null));
         try { ta.setContentType("text/rtf");
-        //boolean ok = job.printDialog();
         boolean done = ta.print();
          if (done) {
              JOptionPane.showMessageDialog(null, "Printing done");
          }else {
              JOptionPane.showMessageDialog(null, "Printing failed");
          }
-             //job.print();
          }catch (PrinterException ex) {
             JOptionPane.showMessageDialog(null, "Error while printing");
         }
     }
 
-
-
-
-   /* @Override
-    public int print(Graphics g, PageFormat pf, int pageIndex) throws PrinterException {
-
-        Graphics2D g2d = (Graphics2D)g;
-        g2d.translate(pf.getImageableX(), pf.getImageableY());
-        return 0;
-    }  */
 }
